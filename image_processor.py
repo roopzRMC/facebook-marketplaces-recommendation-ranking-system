03c5e53b957ad31b43d2d605795066cd7d3fea2d
Rupert Coghlan <r.coghlan@gmail.com>
2023-09-25 15:50:34 +0000

    converting classifier and feature extractor to .py files

---
+++ image_processor.py
@@ -0,0 +1,146 @@
+"""
+Module Name: image_processor
+Author: Rupert Coghlan
+Date: 21/09/2023
+Description: This module creates an embeddings JSON
+"""
+
+
+import datetime  # Importing datetime module for working with dates and times.
+import json  # Importing json module for JSON manipulation.
+import matplotlib.pyplot as plt  # Importing matplotlib.pyplot for data visualization.
+import numpy as np  # Importing numpy for numerical computations.
+import os  # Importing os module for operating system-related tasks.
+import pandas as pd  # Importing pandas for data manipulation and analysis.
+import requests  # Importing requests for making HTTP requests.
+from PIL import Image  # Importing Image from the PIL (Pillow) library for image processing.
+import time  # Importing time module for working with timestamps.
+
+import torch  # Importing the PyTorch library for deep learning.
+import torch.nn as nn  # Importing neural network modules from PyTorch.
+import torch.nn.functional as F  # Importing functional operations for PyTorch neural networks.
+
+import torchvision  # Importing the torchvision library for computer vision tasks.
+import torchvision.transforms as transforms  # Importing data transformation functions.
+from torch.utils.data import Dataset, DataLoader  # Importing data loading utilities from PyTorch.
+from torchvision import datasets, models  # Importing pre-trained models and datasets for computer vision.
+
+from torchvision.models.resnet import *  # Importing ResNet models and related modules.
+from torchvision.models.resnet import BasicBlock, Bottleneck  # Importing specific ResNet building blocks.
+
+# Empty the gpu cache
+torch.cuda.empty_cache()
+
+# Determine the device (GPU or CPU) available for computation - ensure that cuda is displayed
+device = torch.device("this session is using cuda as a torch device") if torch.cuda.is_available() else torch.device("this session is using a cpu as a torch device")
+print(device)
+
+## Creating a train dataset class
+class ItemsTrainDataSet(Dataset):
+    def __init__(self):
+        super().__init__()
+        self.examples = self._load_examples()
+        self.pil_to_tensor = transforms.ToTensor()
+        self.resize = transforms.Resize((225,225))
+        #self.rgbify = transforms.Lambda(lambda x: x.repeat(3, 1, 1) if x.size(0)==1 else x)
+
+    def _load_examples(self):
+        class_names = os.listdir('pytorch_images_tv/train')
+        class_encoder = {class_name: idx for idx, class_name in enumerate(class_names)}
+        class_decoder = {idx: class_name for idx, class_name in enumerate(class_names)}
+
+        examples_list = []
+        for cl_name in class_names:
+            example_fp = os.listdir(os.path.join('pytorch_images_tv/train',cl_name))
+            example_fp = [os.path.join('pytorch_images_tv/train', cl_name, img_name ) for img_name in example_fp]
+            example = [(img_name, class_encoder[cl_name]) for img_name in example_fp]
+            examples_list.extend(example)
+
+        return examples_list
+
+    def __getitem__(self, idx):
+        img_fp, img_class = self.examples[idx]
+        img = Image.open(img_fp)
+
+        features = self.pil_to_tensor(img)
+        features = self.resize(features)
+        #features = self.rgbify(features)
+
+        return features, img_class
+
+    def __len__(self):
+        return len(self.examples)
+
+traindataset = ItemsTrainDataSet()
+train_loader = DataLoader(dataset = traindataset, batch_size=64)
+
+def process_img(image):
+    pil_to_tensor = transforms.ToTensor()
+    resize = transforms.Resize((225,225))
+    img = Image.open(image).convert('RGB')
+
+    features = pil_to_tensor(img)
+    features = resize(features)
+    features = torch.unsqueeze(features, dim=0)
+    #print(features.shape)
+    return features
+
+r
+
+class ItemFeatureExtractor(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.resnet50 = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_resnet50', pretrained=True)
+        #self.resnet50 = model
+        self.resnet50.fc = torch.nn.Linear(2048,1000)
+
+    def forward(self, X):
+        return F.softmax(self.resnet50(X))
+
+## Instantiate a 1000 neuron feature extracto
+model = ItemFeatureExtractor()
+
+## Instantiate the optimiser used
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
+
+## Load the feature extractor weights
+checkpoint = torch.load('final_weights/weights.pt')
+
+## Load the model state
+model.load_state_dict(checkpoint['model_state_dict'])
+optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+## loop through images to get embeddings
+
+class_list = os.listdir('pytorch_images_tv/train')
+
+#####PROCESS EACH TRAINING IMAGE THROUGH THE MODEL AND WRITE EMBEDDINGS TO A DICTIONARY######
+
+images_dir = 'pytorch_images_tv/train'
+embeddings_dict = {}
+
+for cat in class_list:
+  for i in os.listdir(os.path.join(images_dir, cat)):
+    image_fp = os.path.join(images_dir, cat, i)
+    batch_embeddings = model(process_img(os.path.join(images_dir, cat, i)))
+    batch_embeddings = batch_embeddings.view(batch_embeddings.size(0), -1)
+
+    for emb, file_name in zip(batch_embeddings, image_fp):
+        # Get the image ID from the file name (assuming it is the filename without extension)
+        image_id = image_fp.split(".")[0]
+
+        # Create a dictionary for each embedding entry
+        embeddings_dict[image_id] = emb.tolist()
+
+#### Write the embeddings dictionary to a JSON file
+# Specify the path to your JSON file
+json_file_path = "final_embeddings_sep.json"
+
+# Open the file in write mode
+with open(json_file_path, 'w') as file:
+    # Write the dictionary to the JSON file
+    json.dump(embeddings_dict, file)
+
+## Write the json embeddings to a final
+with open('final_embeddings_sep.json', "r") as json_file:
+  data_dict = json.load(json_file)
---
+++ resnet50_classifier.py
@@ -0,0 +1,407 @@
+"""
+Module Name: resnet50_classifier
+Author: Rupert Coghlan
+Date: 21/09/2023
+Description: This script instantiates a pretrained resnet 50 and trains on a custom datset of 13 classes. The weights are saved in model evaluation
+"""
+
+# Import datetime and time for date and time operations
+import datetime
+import time
+
+# Import json for JSON manipulation
+import json
+
+# Import os for file-related activities and folder navigation
+import os
+
+# Import the requests library for HTTP requests
+import requests
+
+# Import warnings for managing warnings
+import warnings
+warnings.filterwarnings('ignore')
+
+# Import NumPy for numerical operations
+import numpy as np
+
+# Import Pandas for data manipulation
+import pandas as pd
+
+# Import the Python Imaging Library (PIL) for image processing
+from PIL import Image
+
+# Import PyTorch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# Import specific components from torchvision.models.resnet
+from torchvision.models.resnet import BasicBlock, Bottleneck
+
+# Import PyTorch Dataset and DataLoader for data handling
+from torch.utils.data import Dataset, DataLoader
+
+# Import PyTorch torchvision.transforms for image transformations during training
+import torchvision.transforms as transforms
+
+# Import PyTorch torchvision.datasets and models
+from torchvision import datasets, models
+
+# Import torchvision for additional image-related functionality
+import torchvision
+
+# Import SummaryWriter from torch.utils.tensorboard for TensorBoard logging
+from torch.utils.tensorboard import SummaryWriter
+
+# Clear GPU cache using torch.cuda.empty_cache()
+torch.cuda.empty_cache()
+
+# Determine the device (GPU or CPU) available for computation - ensure that cuda is displayed
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+print(device)
+
+## Creating a train dataset class
+class ItemsTrainDataSet(Dataset):
+    """
+    A custom dataset class for image training data.
+    
+    This class inherits from the PyTorch Dataset class and will 
+    load and preprocess image training data.
+
+    Attributes
+    ----------
+    None
+
+    Methods
+    -------
+    __init__():
+        Initializes a new ItemsTrainDataSet instance.
+    _load_examples():
+        Loads and prepares a list of image examples.
+    __getitem__(idx):
+        Retrieves an image and its associated class label by index.
+    __len__():
+        Returns the total number of examples in the dataset.
+    """
+    def __init__(self):
+        """
+        Initialize a new ItemsTrainDataSet instance.
+
+        This constructor sets up the necessary transformations for image preprocessing.
+        """        
+        super().__init__()
+        self.examples = self._load_examples()
+        self.pil_to_tensor = transforms.ToTensor()
+        self.resize = transforms.Resize((225,225))
+        #self.rgbify = transforms.Lambda(lambda x: x.repeat(3, 1, 1) if x.size(0)==1 else x)
+
+    def _load_examples(self):
+        """
+        Load and prepare a list of image examples.
+
+        This method scans the training directory, assigns class labels,
+        and collects a list of image file paths.
+
+        Returns
+        -------
+        list
+            A list of tuples containing image file paths and their class labels.
+        """        
+        class_names = os.listdir('pytorch_images_tv/train')
+        class_encoder = {class_name: idx for idx, class_name in enumerate(class_names)}
+        class_decoder = {idx: class_name for idx, class_name in enumerate(class_names)}
+
+        examples_list = []
+        for cl_name in class_names:
+            example_fp = os.listdir(os.path.join('pytorch_images_tv/train',cl_name))
+            example_fp = [os.path.join('pytorch_images_tv/train', cl_name, img_name ) for img_name in example_fp]
+            example = [(img_name, class_encoder[cl_name]) for img_name in example_fp]
+            examples_list.extend(example)
+
+        return examples_list
+
+    def __getitem__(self, idx):
+        """
+        Retrieve an image and its associated class label by index.
+
+        Parameters
+        ----------
+        idx : int
+            The index of the item to retrieve.
+
+        Returns
+        -------
+        tuple
+            A tuple containing the preprocessed image and its class label.
+        """        
+        img_fp, img_class = self.examples[idx]
+        img = Image.open(img_fp)
+
+        features = self.pil_to_tensor(img)
+        features = self.resize(features)
+        #features = self.rgbify(features)
+
+        return features, img_class
+
+    def __len__(self):
+        """
+        Return the total number of examples in the dataset.
+
+        Returns
+        -------
+        int
+            The total number of examples in the dataset.
+        """        
+        return len(self.examples)
+
+## Creates a validation dataset class
+class ItemsValDataSet(Dataset):
+    """
+    A custom dataset class for image validation data.
+    
+    This class inherits from the PyTorch Dataset class and is designed
+    for loading and preprocessing image validation data.
+
+    Attributes
+    ----------
+    None
+
+    Methods
+    -------
+    __init__():
+        Initializes a new ItemsValDataSet instance.
+    _load_examples():
+        Loads and prepares a list of image examples.
+    __getitem__(idx):
+        Retrieves an image and its corresponding class label by index.
+    __len__():
+        Returns the total number of examples in the dataset.
+    """
+    def __init__(self):
+        """
+        Initialize a new ItemsValDataSet instance.
+
+        This constructor sets up the necessary transformations for image preprocessing.
+        """
+        super().__init__()
+        self.examples = self._load_examples()
+        self.pil_to_tensor = transforms.ToTensor()
+        self.resize = transforms.Resize((225,225))
+        #self.rgbify = transforms.Lambda(lambda x: x.repeat(3, 1, 1) if x.size(0)!=1 else x)
+
+    def _load_examples(self):
+        """
+        Load and prepare a list of image examples.
+
+        This method scans the validation directory, assigns class labels,
+        and collects a list of image file paths.
+
+        Returns
+        -------
+        list
+            A list of tuples containing image file paths and their class labels.
+        """        
+        class_names = os.listdir('pytorch_images_tv/val')
+        class_encoder = {class_name: idx for idx, class_name in enumerate(class_names)}
+        class_decoder = {idx: class_name for idx, class_name in enumerate(class_names)}
+        examples_list = []
+
+        for cl_name in class_names:
+            example_fp = os.listdir(os.path.join('pytorch_images_tv/val',cl_name))
+            example_fp = [os.path.join('pytorch_images_tv/val', cl_name, img_name ) for img_name in example_fp]
+            example = [(img_name, class_encoder[cl_name]) for img_name in example_fp]
+            examples_list.extend(example)
+
+        return examples_list
+
+    def __getitem__(self, idx):
+        """
+        Retrieve an image and its corresponding class label by index.
+
+        Parameters
+        ----------
+        idx : int
+            The index of the item to retrieve.
+
+        Returns
+        -------
+        tuple
+            A tuple containing the preprocessed image and its class label.
+        """        
+        img_fp, img_class = self.examples[idx]
+        img = Image.open(img_fp)
+
+        features = self.pil_to_tensor(img)
+        features = self.resize(features)
+        #features = self.rgbify(features)
+
+        return features, img_class
+
+    def __len__(self):
+        """
+        Return the total number of examples in the dataset.
+
+        Returns
+        -------
+        int
+            The total number of examples in the dataset.
+        """        
+        return len(self.examples)
+
+
+# Create the traindataset object 
+traindataset = ItemsTrainDataSet()
+
+# Create the validation dataset object
+valdataset = ItemsValDataSet()
+
+## Created a classifier based on the RESNET50 pretrained model
+
+class ItemClassifier(torch.nn.Module):
+    """
+    A custom nn.Module class housing the classifier which is
+    based on a gpu-derived pretrained resnet50 from NVIDA torchhub
+    
+    Attributes
+    ----------
+    None
+    
+    
+    Methods
+    -------
+    __init__():
+        Initialises the classifier and loads the model from the torchhub
+        unless it is able to detect a cached instance.
+        Replaces the final layer with a 13 class output
+    
+    forward():
+        Initiates the forward pass
+        
+    """    
+    def __init__(self):
+        super().__init__()
+        self.resnet50 = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_resnet50', pretrained=True)
+        #self.resnet50 = model
+        self.resnet50.fc = torch.nn.Linear(2048,13)
+
+    def forward(self, X):
+        return F.softmax(self.resnet50(X))
+
+def train(model,traindataloader, valdataloader, epochs):
+    """
+    Train the modified pretrained resnet50 classifier.
+    
+    The model weights are written after each epoch to model_evaluation in
+    the current working directory
+    
+    In addition at the end of each epoch:
+        Validation Accuracy
+        Validation Loss
+        Training Accuracy
+        Training Loss
+    are written to the logs file for the tensorboard to visualise
+    performance
+
+    Parameters
+    ----------
+    model : torch.nn.Module
+        The deep learning model to be trained.
+    traindataloader : torch.utils.data.DataLoader
+        DataLoader for the training dataset.
+    valdataloader : torch.utils.data.DataLoader
+        DataLoader for the validation dataset.
+    epochs : int
+        The number of training epochs.
+
+    Returns
+    -------
+    None
+    """    
+    optimiser = torch.optim.SGD(model.parameters(), lr=0.01)
+    model_path = str(os.path.join('model_evaluation', time.strftime("%Y%m%d-%H%M%S")))
+    os.makedirs(model_path)
+    os.makedirs(os.path.join(model_path, 'weights'))
+
+    global_step = 0
+
+    for epoch in range(epochs):
+        training_loss = 0.0
+        validation_loss = 0.0
+        model.to(device)
+        model.train()
+        tr_num_correct = 0
+        tr_num_examples = 0
+        epoch_combo = 'epoch' + str(epoch)
+        os.makedirs(os.path.join(model_path, 'weights', epoch_combo))
+        for inputs, labels in traindataloader:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            predictions = model(inputs)
+            loss = torch.nn.CrossEntropyLoss()
+            loss = loss(predictions, labels)
+            loss.backward()
+            optimiser.step()
+            model_save_dir = str(os.path.join(model_path, 'weights', epoch_combo, 'weights.pt'))
+            full_path = str(os.getcwd())
+            torch.save({'epoch': epoch,
+                  'model_state_dict': model.state_dict(),
+                  'optimizer_state_dict': optimiser.state_dict()},
+                  str(os.path.join(full_path, model_save_dir)))
+
+            optimiser.zero_grad()
+            training_loss += loss.item() * inputs.size(0)
+            correct = torch.eq(torch.max(F.softmax(predictions, dim=1), dim=1)[1], labels)
+            tr_num_correct += torch.sum(correct).item()
+            tr_num_examples += correct.shape[0]
+        training_loss /= len(traindataloader.dataset)
+        training_accuracy = tr_num_correct / tr_num_examples
+
+        model.eval()
+        val_num_correct = 0
+        val_num_examples = 0
+        for inputs, labels in valdataloader:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            predictions = model(inputs)
+            loss = torch.nn.CrossEntropyLoss()
+            loss = loss(predictions, labels)
+            validation_loss += loss.item() * inputs.size(0)
+            correct = torch.eq(torch.max(F.softmax(predictions, dim =1), dim=1)[1], labels)
+            val_num_correct += torch.sum(correct).item()
+            val_num_examples += correct.shape[0]
+        validation_loss /= len(valdataloader.dataset)
+        validation_accuracy = val_num_correct / val_num_examples
+
+        perf_dict = {}
+        perf_dict[epoch] = {'training_loss': training_loss,
+                            'val_loss': validation_loss,
+                            'training_accuracy': tr_num_correct / tr_num_examples,
+                            'val_accuracy': val_num_correct / val_num_examples}
+
+
+        print('Epoch: {}, Training Loss: {:.2f}, Validation Loss: {:.2f}, train_accuracy = {:.2f},val_accuracy = {:.2f}'.format(
+            epoch,
+            training_loss,
+            validation_loss, 
+            tr_num_correct / tr_num_examples, 
+            val_num_correct / val_num_examples))
+        global_step += 1
+
+## Create the classifier object from the ItemClassifier class
+classifier = ItemClassifier()
+
+## define the layers to unfreeze and then retrain
+layers_to_unfreeze = ['layers.2', 'layers.3']
+
+for name, param in classifier.resnet50.named_parameters():
+    for layer_name in layers_to_unfreeze:
+        if layer_name in name:
+            param.requires_grad = True
+            break
+
+## Create the train and validation loaders
+## Pass the loaders, classifier and desired number of epochs to the train function define above
+train_loader = DataLoader(dataset = traindataset, batch_size=16)
+val_loader = DataLoader(dataset = valdataset, batch_size=16)
+train(classifier, traindataloader= train_loader, valdataloader= val_loader, epochs=150)
