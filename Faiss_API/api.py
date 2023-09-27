4b899fbb83193d213996a5c26bc7367d4ebae90b
Rupert Coghlan <r.coghlan@gmail.com>
2023-09-27 11:18:33 +0100

    amending api.py for brevity

--- Faiss_API/api.py
+++ Faiss_API/api.py
@@ -7,20 +7,20 @@ import torch  # Importing the PyTorch library for deep learning.
 import torch.nn as nn  # Importing neural network modules from PyTorch.
 import torch.nn.functional as F  # Importing functional operations for PyTorch neural networks.
 import torchvision  # Importing the torchvision library for computer vision tasks.
+import image_processor  # Importing a custom module named "image_processor."
 from PIL import Image  # Importing Image from the PIL (Pillow) library for image processing.
 from fastapi import File, UploadFile, Form  # Importing FastAPI modules for file uploads and forms.
 from fastapi.responses import JSONResponse  # Importing FastAPI's JSONResponse for handling JSON responses.
 from pydantic import BaseModel  # Importing Pydantic's BaseModel for defining data models.
-import image_processor  # Importing a custom module named "image_processor."
+
 
 ## Load the feature extractor
-
 class ItemFeatureExtractor(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.resnet50 = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_resnet50', pretrained=True)
-        #self.resnet50 = model
         self.resnet50.fc = torch.nn.Linear(2048,1000)
+
 
     def forward(self, X):
         return F.softmax(self.resnet50(X))
@@ -90,13 +90,16 @@ def predict_image(image: UploadFile = File(...)):
 
 @app.post('/predict/similar_images')
 
-
 def predict_combined(image: UploadFile = File(...)):
     """
     Predict similar images for an uploaded image based on pre-computed embeddings.
 
     This function takes an uploaded image file, processes it to obtain embeddings
     using a pre-trained model, and finds similar images based on the computed embeddings.
+    
+    A flattened array of float 32 vectors is created as well as a matching array of vector ids given the filename submitted
+    
+    
 
     Parameters
     ----------
@@ -110,9 +113,7 @@ def predict_combined(image: UploadFile = File(...)):
     """    
     with open('final_embeddings.json', "r") as json_file:
         data_dict = json.load(json_file)
-    ## Create a flattened array of float32 vectors
     embeddings_array = np.array(list(data_dict.values()), dtype='float32')
-    ## Create a maching array of the vector ids (based on the filenames)
     embeddings_ids = np.array(list(data_dict.keys()))
     pil_image = Image.open(image.file)
     features = image_processor.process_img(pil_image)
